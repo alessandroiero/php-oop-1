2444daa89d83702c17c36576a49aecde8a00247f
alessandroiero <alessandro.iero@outlook.it>
2023-04-17 09:13:47 +0200

    Aggiunti 2 Film

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require_once __DIR__ . "/models/movies.php";
 
 $movies = [
     new Movies("John Wick 4",2023,"Azione, Drammatico, Thriller"),
+    new Movies("Il Cavaliere Oscuro",2008,"Azione, Drammatico, Thriller"),
+    new Movies("Inception",2010,"Azione, Fantascienza, Thriller"),
 ];
 
 var_dump($movies)
